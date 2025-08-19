8d8dd48e213686ca30cc9b60cfeb3d08c13661b6
Amirul <amiruljamari83@gmail.com>
2025-08-20 07:53:51 +0800

    Initial commit

---
+++ config/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Asia/Kuala_Lumpur';
+
+$g_crypto_master_salt     = '9DXzcsf0c08hj7BBiIFIM/gY8STy4ag6wKV6zRmrRME=';
+
+$g_path                   = 'http://localhost:82/mantisbt/';
